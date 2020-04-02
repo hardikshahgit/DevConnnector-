5c0aad42f0e7eab0e786b47e1b70c941b107935d
hardikshahgit <roophardik@gmail.com>
2020-04-02 15:42:31 +0530

    Finished with Dashboard actions

---
+++ client/src/components/dashboard/DashboardActions.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {EditProfile} from '../profile-forms/EditProfile';
+
+const DashboardActions = props => {
+    return (
+        <div class="dash-buttons">
+            <Link to="/edit-profile" class="btn btn-light"><i class="fas fa-user-circle text-primary"> </i> 
+                Edit Profile
+            </Link>
+            <Link to="/add-experience" class="btn btn-light"><i class="fab fa-black-tie text-primary"> </i>
+                 Add Experience
+            </Link>
+            <Link to="/add-education" class="btn btn-light"><i class="fas fa-graduation-cap text-primary"> </i>
+                Add Education
+            </Link>
+        </div>
+    )
+}
+
+DashboardActions.propTypes = {
+
+}
+
+export default DashboardActions
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,7 @@ const {check, validationResult} = require('express-validator/check');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');  
+const Post  = require('../../models/Post');  
 const request = require('request');
 const config = require('config');
 
@@ -122,8 +123,8 @@ router.get('/user/:user_id', async (req, res) => {
 // @access  Private
 router.delete('/', auth, async (req, res) => {
     try {
-        //@todo - remove user profile 
-        
+        //remove user posts
+        await Post.deleteMany({user: req.user.id});
         //remove profile
         await Profile.findOneAndRemove({user: req.user.id});
         //remove user
